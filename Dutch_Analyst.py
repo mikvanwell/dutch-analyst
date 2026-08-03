875d8e3a5ff21012ed11bae8adf5fc87cd928fe2
mikvanwell <mikvanwell@outlook.com>
2026-08-03 20:46:38 +0000

    small change

--- Dutch_Analyst.py
+++ Dutch_Analyst.py
@@ -11,8 +11,8 @@ st.write("**Welcome to the site with everything you need to analyse Dutch footba
 st.write("Wondering which Eredivisie teams have the best underlying data?")
 st.page_link("pages/Team_Data.py", label="➡️ Go to Team Data 📊")
 
-st.write("Do you need help with your Fantasy Eredivisie squad?")
-st.page_link("pages/FDR_Schedule.py", label="➡️ Go to our Fantasy Hub 🔮")
+st.write("Want to know which teams face the easiest schedule?")
+st.page_link("pages/FDR_Schedule.py", label="➡️ Go to our Fantasy Schedule 🔮")
 
 st.write("Want to know which players are expected to score most points?")
 st.page_link("pages/Player_Analysis.py", label="➡️ Go to Player Analysis ⚽️")
